6c8c73ef6dc7ba521ae6ca5255011fc9a0e46702
Roy Zhang <royzhangno7@gmail.com>
2018-04-23 22:16:31 -0400

    discard comment

--- secretary_prob.py
+++ secretary_prob.py
@@ -13,19 +13,9 @@ for j in range(10000):
     for i in range(numcandidate):
         candidate.append(i)
     random.shuffle(candidate)
-    # the value of the list is the rank of the candidates
-
-    # print ("rank ")
-    # print (candidate)
-
     samplesize = round(len(candidate) * math.exp(-1))
-    # another method is selecting sqrt(n) sample
-    # print ("sample size is " + str(samplesize))
-
     sample = candidate[ : samplesize]
-
     benchmark = min(sample)
-    # print ("best of the sample is " + str(benchmark))
     best = 0
     index = samplesize
     while index <= len(candidate) - 1:
@@ -35,13 +25,7 @@ for j in range(10000):
                 count += 1
             break
         index += 1
-    # if candidate[best] <= benchmark:
-        # count += 1
-        # print ("Best candidate found is " + str(index+1) + " with talent " + str(candidate[index]))
-    # else:
-        # print ("Could not find a best candidate")
-    # except:
-        # print("Could not find a best candidate")
+
 end = time.clock()
 print("probability is ", count / numcandidate)
 print("running time is ", end - start)
